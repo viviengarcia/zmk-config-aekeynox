b2a8e0664eba5109e7d14f2d610647921172ea5b
Fabien Cazenave <fabi1cazenave@users.noreply.github.com>
2026-03-10 08:49:25 +0100

    4-space indentation everywhere (#5)

--- include/selenium/aliases.h
+++ include/selenium/aliases.h
@@ -28,27 +28,27 @@
 // Numbers
 
 #ifdef SHIFTED_NUMBERS
-  #define S_N0  &kp LS(N0)
-  #define S_N1  &kp LS(N1)
-  #define S_N2  &kp LS(N2)
-  #define S_N3  &kp LS(N3)
-  #define S_N4  &kp LS(N4)
-  #define S_N5  &kp LS(N5)
-  #define S_N6  &kp LS(N6)
-  #define S_N7  &kp LS(N7)
-  #define S_N8  &kp LS(N8)
-  #define S_N9  &kp LS(N9)
+    #define S_N0  &kp LS(N0)
+    #define S_N1  &kp LS(N1)
+    #define S_N2  &kp LS(N2)
+    #define S_N3  &kp LS(N3)
+    #define S_N4  &kp LS(N4)
+    #define S_N5  &kp LS(N5)
+    #define S_N6  &kp LS(N6)
+    #define S_N7  &kp LS(N7)
+    #define S_N8  &kp LS(N8)
+    #define S_N9  &kp LS(N9)
 #else
-  #define S_N0  &kp N0
-  #define S_N1  &kp N1
-  #define S_N2  &kp N2
-  #define S_N3  &kp N3
-  #define S_N4  &kp N4
-  #define S_N5  &kp N5
-  #define S_N6  &kp N6
-  #define S_N7  &kp N7
-  #define S_N8  &kp N8
-  #define S_N9  &kp N9
+    #define S_N0  &kp N0
+    #define S_N1  &kp N1
+    #define S_N2  &kp N2
+    #define S_N3  &kp N3
+    #define S_N4  &kp N4
+    #define S_N5  &kp N5
+    #define S_N6  &kp N6
+    #define S_N7  &kp N7
+    #define S_N8  &kp N8
+    #define S_N9  &kp N9
 #endif
 
 // Non-Alpha Actions
